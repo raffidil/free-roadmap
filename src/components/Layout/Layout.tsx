2e2d39f52354a9bec83af25966b1dbb2c68c4ff7
Raffi Dilanchian <raffidilanchian@gmail.com>
2022-10-08 14:26:28 +0330

    Add page title

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@mui/material";
+import Head from "next/head";
 import Header from "../Header";
 import TopLevelDialogs from "../TopLevelDialogs";
 import styles from "./Layout.module.scss";
@@ -6,6 +7,9 @@ import styles from "./Layout.module.scss";
 const Layout: React.FC<{ children?: any }> = ({ children }) => {
   return (
     <Container className={styles.container} maxWidth="lg">
+      <Head>
+        <title>Free RoadMap</title>
+      </Head>
       <Header />
       <TopLevelDialogs />
       {children}
