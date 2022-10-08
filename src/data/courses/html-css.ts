2d6217bcc606ad332d0863d98da8c401f74a8b62
Samim Pezeshki <psamim@gmail.com>
2022-10-08 19:40:05 +0330

    Update html courses

--- src/data/courses/html-css.ts
+++ src/data/courses/html-css.ts
@@ -9,20 +9,6 @@ const databaseCourse: Course = {
   color: "lightGreen",
   lessons: [
     {
-      id: "intro",
-      name: "Introduction",
-      description: "History of Web (HTML/CSS/JS)",
-      weekNo: 1,
-      resources: [
-        {
-          id: "web-intro",
-          label: "History of Web",
-          url: "https://web-intro-present.vercel.app/",
-          type: "link",
-        },
-      ],
-    },
-    {
       id: "basic-syntax",
       name: "Basic syntax",
       description: "Basic HTML tags: h1, h1, p, comment, main and nesting.",
@@ -37,10 +23,35 @@ const databaseCourse: Course = {
       ],
     },
     {
-      id: "basic-tags",
-      name: "More tags",
-      description:
-        "More tags: img self closing tag, attributes, anchor tag, section tag.",
+      id: "basic-tags-i",
+      name: "Image",
+      description: "img self closing tag and attributes",
+      weekNo: 2,
+      resources: [
+        {
+          id: "free2",
+          label: "Cat photo app project: steps 1 to 20",
+          url: "https://www.freecodecamp.org/learn/2022/responsive-web-design/#learn-html-by-building-a-cat-photo-app",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "basic-tags-a",
+      name: "Anchor",
+      weekNo: 2,
+      resources: [
+        {
+          id: "free2",
+          label: "Cat photo app project: steps 1 to 20",
+          url: "https://www.freecodecamp.org/learn/2022/responsive-web-design/#learn-html-by-building-a-cat-photo-app",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "basic-tags-s",
+      name: "Section",
       weekNo: 2,
       resources: [
         {
@@ -53,8 +64,21 @@ const databaseCourse: Course = {
     },
     {
       id: "tags2",
-      name: "Even more tags",
-      description: "li, ol, figure, em and strong tag",
+      name: "Lists",
+      description: "li, ol",
+      weekNo: 3,
+      resources: [
+        {
+          id: "free3",
+          label: "Cat photo app project: steps 20 to 34",
+          url: "https://www.freecodecamp.org/learn/2022/responsive-web-design/#learn-html-by-building-a-cat-photo-app/step-20",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "tags2-fig",
+      name: "Figure",
       weekNo: 3,
       resources: [
         {
@@ -69,7 +93,20 @@ const databaseCourse: Course = {
       id: "forms",
       name: "Forms",
       description:
-        "Form, input, button, radio buttons, labels, input value and name, fieldset, inline and block-level elements",
+        "Form, input, button, radio buttons, labels, input value and name, fieldset",
+      weekNo: 4,
+      resources: [
+        {
+          id: "free4",
+          label: "Cat photo app project: steps 35 to 51",
+          url: "https://www.freecodecamp.org/learn/2022/responsive-web-design/#learn-html-by-building-a-cat-photo-app/step-35",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "forms-e",
+      name: "Inline and block-level",
       weekNo: 4,
       resources: [
         {
@@ -82,9 +119,35 @@ const databaseCourse: Course = {
     },
     {
       id: "forms2",
-      name: "More on forms",
-      description:
-        "Checkbox, checked attributes, footer, body, head, title, doctype, meta",
+      name: "Checkbox",
+      description: "Checkbox, checked attributes",
+      weekNo: 5,
+      resources: [
+        {
+          id: "free5",
+          label: "Cat photo app project: steps 52 to the end.",
+          url: "https://www.freecodecamp.org/learn/2022/responsive-web-design/#learn-html-by-building-a-cat-photo-app/step-52",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "forms-a",
+      name: "Footer",
+      weekNo: 5,
+      resources: [
+        {
+          id: "free5",
+          label: "Cat photo app project: steps 52 to the end.",
+          url: "https://www.freecodecamp.org/learn/2022/responsive-web-design/#learn-html-by-building-a-cat-photo-app/step-52",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "forms-b",
+      name: "Body and head",
+      description: "Footer, body, head, title, doctype, meta",
       weekNo: 5,
       resources: [
         {
