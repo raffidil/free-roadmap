5369155d699fa5e632e1b22b0582376df1a242c0
sshervin <s.salmanzadeh@fomf.org>
2022-10-11 23:36:48 +0330

    update-fundamental-course-content

--- src/data/courses/fundamental.ts
+++ src/data/courses/fundamental.ts
@@ -5,68 +5,81 @@ const fundamentalCourse: Course = {
   id: "fundamental",
   name: "Web Fundamentals",
   tag: [tags.programming],
-  color: "red",
+  color: "orange",
   lessons: [
     {
       id: "w1a",
       weekNo: 1,
-      name: "Computer hardware",
+      name: "Introduction and acquaintance",
+      
     },
     {
       id: "w1b",
       weekNo: 1,
-      name: "Computer software",
+      name: "Computer hardware",
+      resources: [
+        {
+          id: "1",
+          label: "System Information",
+          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work",
+          type: "link",
+        },
+      ],
     },
     {
       id: "w2a",
       weekNo: 2,
       name: "Computer networks",
+      resources: [
+        {
+          id: "1",
+          label: "what is IP?",
+          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work",
+          type: "link",
+        },
+      ],
     },
     {
       id: "w2b",
       weekNo: 3,
-      name: "How Internet works?",
+      name: "VM",
       resources: [
         {
           id: "1",
-          label: "How does the Internet work?",
+          label: "what is server?",
+          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "w4a",
+      weekNo: 4,
+      name: "Computer software",
+      resources: [
+        {
+          id: "1",
+          label: "What does backend mean",
           url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work",
           type: "link",
         },
         {
           id: "2",
-          label: "What are hyperlinks?",
-          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_are_hyperlinks",
+          label: "What does frontend mean",
+          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work",
           type: "link",
         },
+      ],
+    },
+    {
+      id: "w5a",
+      weekNo: 5,
+      name: "Computer network",
+      resources: [
         {
-          id: "3",
-          label: "What is a Domain Name?",
-          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_is_a_domain_name",
-          type: "link",
-        },
-        {
-          id: "4",
-          label: "What is a URL?",
-          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/What_is_a_URL",
-          type: "link",
-        },
-        {
-          id: "5",
-          label: "HTTP GET Method",
-          url: "https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/GET",
-          type: "link",
-        },
-        {
-          id: "6",
-          label: "HTTP POST Method",
-          url: "https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/POST",
-          type: "link",
-        },
-        {
-          id: "7",
-          label: "HTTP response status codes",
-          url: "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status",
+          id: "1",
+          label: "what is API?",
+          url: "https://developer.mozilla.org/en-US/docs/Learn/Common_questions/How_does_the_Internet_work",
           type: "link",
         },
       ],
@@ -74,16 +87,6 @@ const fundamentalCourse: Course = {
     {
       id: "w6a",
       weekNo: 6,
-      name: "What is Web?",
-    },
-    {
-      id: "w7a",
-      weekNo: 7,
-      name: "What does backend mean?",
-    },
-    {
-      id: "w10a",
-      weekNo: 10,
       name: "What is API?",
       resources: [
         {
@@ -95,49 +98,54 @@ const fundamentalCourse: Course = {
       ],
     },
     {
-      id: "w10b",
-      weekNo: 10,
-      name: "RESTful API",
-    },
-    {
-      id: "w11a",
-      weekNo: 11,
-      name: "Linux",
-    },
-    {
-      id: "w11b",
-      weekNo: 11,
-      name: "Running Linux in a virtual machine",
-    },
-    {
-      id: "w12a",
-      weekNo: 12,
-      name: "Linux users and groups",
+      id: "w7b",
+      weekNo: 7,
+      name: "Computer software",
       resources: [
         {
           id: "1",
-          label: "How to manage users and groups in Linux",
-          url: "https://www.redhat.com/sysadmin/linux-user-group-management",
+          label: "Security",
+          url: "https://www.ibm.com/cloud/learn/api",
           type: "link",
         },
       ],
     },
     {
-      id: "w12b",
-      weekNo: 12,
-      name: "Linux permissions",
+      id: "w10a",
+      weekNo: 10,
+      name: "Computer software",
+      resources: [
+        {
+          id: "1",
+          label: "OS",
+          url: "https://www.ibm.com/cloud/learn/api",
+          type: "link",
+        },
+      ],
+    },
+    {
+      id: "w13a",
+      weekNo: 13,
+      name: "VM",
       resources: [
         {
           id: "1",
           label:
-            "How to manage Linux permissions for users, groups, and others",
+            "VPS",
+          url: "https://www.redhat.com/sysadmin/manage-permissions",
+          type: "link",
+        },
+        {
+          id: "2",
+          label:
+            "SSH",
           url: "https://www.redhat.com/sysadmin/manage-permissions",
           type: "link",
         },
       ],
     },
     {
-      id: "w14b",
+      id: "w14a",
       weekNo: 14,
       name: "Install packages using apt",
       resources: [
@@ -151,132 +159,44 @@ const fundamentalCourse: Course = {
       ],
     },
     {
-      id: "w14c",
-      weekNo: 14,
-      name: "Monitor processes",
+      id: "w15a",
+      weekNo: 15,
+      name: "Linux",
       resources: [
         {
           id: "1",
-          label: "ps(1)",
+          label: "Commands(1)",
           url: "https://linux.die.net/man/1/ps",
           type: "link",
         },
-        {
-          id: "2",
-          label: "Linux PS command with examples",
-          url: "https://en.linuxteaching.com/article/linux_ps_command_with_examples",
-          type: "link",
-        },
       ],
     },
     {
-      id: "w14d",
-      weekNo: 14,
-      name: "Terminate a process",
+      id: "16a",
+      weekNo: 16,
+      name: "Linux",
       resources: [
         {
           id: "1",
-          label: "kill(1)",
+          label: "Commands(2)",
           url: "https://linux.die.net/man/1/kill",
           type: "link",
         },
-        {
-          id: "2",
-          label: "killall(1)",
-          url: "https://linux.die.net/man/1/killall",
-          type: "link",
-        },
       ],
     },
     {
-      id: "w14e",
-      weekNo: 14,
-      name: "Pipes",
+      id: "w17a",
+      weekNo: 17,
+      name: "Linux",
       resources: [
         {
           id: "1",
-          label: "pipeline in linux/unix with examples",
+          label: "VIM",
           url: "https://www.linuxcommands.site/linux-system-commands/pipeline-in-linux-unix-with-examples/",
           type: "link",
         },
-        {
-          id: "2",
-          label: "How Do You Pipe the Output of a Command to a File in Linux",
-          url: "https://linuxhint.com/how-do-you-pipe-the-output-of-a-command-to-a-file-in-linux/",
-          type: "link",
-        },
       ],
     },
-    {
-      id: "w14f",
-      weekNo: 14,
-      name: "grep",
-      resources: [
-        {
-          id: "1",
-          label: "How To Use grep Command In Linux/UNIX",
-          url: "https://phoenixnap.com/kb/grep-command-linux-unix-examples",
-          type: "link",
-        },
-      ],
-    },
-    {
-      id: "w14g",
-      weekNo: 14,
-      name: "find",
-      resources: [
-        {
-          id: "1",
-          label: "How to Use the find Command in Linux",
-          url: "https://www.howtogeek.com/771399/how-to-use-the-find-command-in-linux/",
-          type: "link",
-        },
-      ],
-    },
-    {
-      id: "w15a",
-      weekNo: 15,
-      name: "bash scripts",
-      resources: [
-        {
-          id: "1",
-          label: "How to Create and Use Bash Scripts",
-          url: "https://www.taniarascia.com/how-to-create-and-use-bash-scripts/",
-          type: "link",
-        },
-      ],
-    },
-    {
-      id: "w15b",
-      weekNo: 15,
-      name: "Regular Expressions",
-      resources: [
-        {
-          id: "1",
-          label: "Linux Regular Expression",
-          url: "https://www.educba.com/linux-regular-expression/",
-          type: "link",
-        },
-      ],
-    },
-    {
-      id: "w15c",
-      weekNo: 15,
-      name: "Environment Variables",
-      resources: [
-        {
-          id: "1",
-          label: "How to Set Environment Variables in Linux",
-          url: "https://phoenixnap.com/kb/linux-set-environment-variable",
-          type: "link",
-        },
-      ],
-    },
-    {
-      id: "w16c",
-      weekNo: 16,
-      name: "Introduction to Python",
-    }
   ],
 };
 
